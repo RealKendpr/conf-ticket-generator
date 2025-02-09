6d1439c20c841d8e944e5c34140ba99983341cc3
kendpr <real.kendpr@gmail.com>
2025-02-09 23:25:36 +0800

    added logo and heading copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="font-display">Hello World 2025</div>;
+  return (
+    <div className="px-[4%] pt-8 pb-28">
+      <img className="mx-auto mb-11" src="/images/logo-full.svg" alt="" />
+      <h1 className="text-center text-[1.75rem] leading-[1.1] font-medium text-white">
+        Your Journey to Coding Conf 2025 Starts Here!
+      </h1>
+      <h2 className="mt-8 mb-11 text-center text-xl leading-[1.2] text-white">
+        Secure your spot at next year's biggest coding conference.
+      </h2>
+    </div>
+  );
 }
 
 export default App;
