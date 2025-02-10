c60bc97e9104b0bf851c9356f1df7fbd97503c0d
kendpr <real.kendpr@gmail.com>
2025-02-10 14:48:42 +0800

    styled drag n drop and preview

--- src/components/dropzone.tsx
+++ src/components/dropzone.tsx
@@ -23,16 +23,28 @@ export default function Dropzone() {
   });
 
   return (
-    <div className="container">
-      <div {...getRootProps({ className: "dropzone" })}>
+    <div className="container mb-7">
+      <p className="text-neutral-0 text-xl">Upload Avatar</p>
+      <div
+        {...getRootProps({
+          className:
+            "dropzone border-neutral-0 bg-slate-500/25 border border-dashed py-5 text-center rounded-xl cursor-pointer my-3",
+        })}
+      >
         <input {...getInputProps()} />
         {files.map((file) => (
-          <div key={file.name}>
+          <div
+            className="mx-auto grid size-[51px] place-items-center rounded-xl border-2 border-neutral-300/50"
+            key={file.name}
+          >
             <img src={file.preview} alt="" />
           </div>
         ))}
         <p className="text-neutral-0">Drag and drop or click to upload</p>
       </div>
+      <p className="text-neutral-0 text-xs">
+        Upload your photo (JPG or PNG, max size: 500KB).
+      </p>
     </div>
   );
 }
