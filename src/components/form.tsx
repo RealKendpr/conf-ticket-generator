441888fbde1877beb83247fb0a513b97df64c95b
kendpr <real.kendpr@gmail.com>
2025-02-10 13:45:38 +0800

    added placeholders for inouts and submit button

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -3,9 +3,27 @@ import Input from "./Input";
 export default function Form() {
   return (
     <form action="/">
-      <Input label="Full Name" type="text" name="fullName"></Input>
-      <Input label="Email Address" type="email" name="emailAddress"></Input>
-      <Input label="Github Username" type="text" name="ghUsername"></Input>
+      <div className="grid gap-y-7">
+        <Input label="Full Name" type="text" name="fullName"></Input>
+        <Input
+          label="Email Address"
+          type="email"
+          name="emailAddress"
+          placeholder="example@email.com"
+        ></Input>
+        <Input
+          label="Github Username"
+          type="text"
+          name="ghUsername"
+          placeholder="@yourusername"
+        ></Input>
+      </div>
+      <button
+        className="mt-6 w-full cursor-pointer rounded-xl bg-orange-500 pt-5 pb-4 text-xl font-bold text-neutral-900"
+        type="submit"
+      >
+        Generate My Ticket
+      </button>
     </form>
   );
 }
