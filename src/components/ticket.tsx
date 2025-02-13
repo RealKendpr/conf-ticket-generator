2cb0cead0348483158a1a107d2da29dcdfe958f2
kendpr <real.kendpr@gmail.com>
2025-02-13 20:33:31 +0800

    layout of ticket using grid

--- src/components/ticket.tsx
+++ src/components/ticket.tsx
@@ -21,19 +21,20 @@ export default function Ticket({ formSubmitted }: { formSubmitted: boolean }) {
   }, [formSubmitted]);
 
   return (
-    <div>
-      <div>
-        <div>
-          <img src="/images/logo-full.svg" alt="" />
-          <div>Jan 31, 2025 / Austin, Tx</div>
-        </div>
-        <div>
-          <img src={formData?.avatar.preview} alt="" />
-          <div>{formData?.name}</div>
-          <div>{formData?.ghUserName}</div>
-        </div>
+    // https://cssgrid-generator.netlify.app/
+    <div className="grid max-h-[161px] w-full grid-cols-[1fr_19%] grid-rows-2 gap-0 bg-[url(/images/pattern-ticket.svg)] bg-cover bg-center bg-no-repeat">
+      <div className="col-start-1 col-end-2 row-start-1 row-end-2">
+        <img src="/images/logo-full.svg" alt="" />
+        <div>Jan 31, 2025 / Austin, Tx</div>
       </div>
-      <div>#01609</div>
+      <div className="col-start-1 col-end-2 row-start-2 row-end-3">
+        <img src={formData?.avatar.preview} alt="" />
+        <div>{formData?.name}</div>
+        <div>{formData?.ghUserName}</div>
+      </div>
+      <div className="col-start-2 col-end-3 row-start-1 row-end-3 grid place-items-center">
+        <p className="rotate-90">#01609</p>
+      </div>
     </div>
   );
 }
