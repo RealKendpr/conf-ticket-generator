33ade7c916d97e5156430dfe1b68482c4a4dad07
kendpr <real.kendpr@gmail.com>
2025-02-17 00:10:14 +0800

    Text gradient

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -12,10 +12,15 @@ export default function Headings({
       {formSubmitted ? (
         <>
           <h1 className="text-neutral-0 text-center text-[1.75rem] leading-[1.1] font-bold md:text-[4.03vw]">
-            Congrat's <span>{name}!</span> Your ticket is ready.
+            Congrat's{" "}
+            <span className="from-orange-gradient to-neutral-0 bg-linear-to-r bg-clip-text text-transparent">
+              {name}!
+            </span>{" "}
+            Your ticket is ready.
           </h1>
           <h2 className="mt-8 mb-11 text-center text-xl leading-[1.2] text-neutral-300 md:mt-6 md:text-[1.53vw]">
-            We've mailed your ticket to <span>{email}</span> and will send
+            We've mailed your ticket to{" "}
+            <span className="text-orange-500">{email}</span> and will send
             updates in the run up to the event.
           </h2>
         </>
