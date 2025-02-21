fc129a2e09efaf41f820c30293d98312d0935ca4
kendpr <real.kendpr@gmail.com>
2025-02-22 07:29:02 +0800

    better fluid texts using utopia.fyi

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -11,14 +11,14 @@ export default function Headings({
     <>
       {formSubmitted ? (
         <>
-          <h1 className="text-neutral-0 text-center text-[1.75rem] leading-[1.1] font-bold md:text-[4.03vw]">
+          <h1 className="text-neutral-0 text-2 text-center leading-[1.1] font-bold">
             Congrat's{" "}
             <span className="from-orange-gradient to-neutral-0 bg-linear-to-r bg-clip-text text-transparent">
               {name}!
             </span>{" "}
             Your ticket is ready.
           </h1>
-          <h2 className="mt-8 mb-11 text-center text-xl leading-[1.2] text-neutral-300 md:mt-6 md:text-[1.53vw]">
+          <h2 className="text-0 mt-8 mb-11 text-center leading-[1.2] text-neutral-300 md:mt-6">
             We've mailed your ticket to{" "}
             <span className="text-orange-500">{email}</span> and will send
             updates in the run up to the event.
@@ -26,10 +26,10 @@ export default function Headings({
         </>
       ) : (
         <>
-          <h1 className="text-neutral-0 text-center text-[1.75rem] leading-[1.1] font-bold md:text-[4.03vw]">
+          <h1 className="text-neutral-0 text-2 text-center leading-[1.1] font-bold">
             Your Journey to Coding Conf 2025 Starts Here!
           </h1>
-          <h2 className="mt-8 mb-11 text-center text-xl leading-[1.2] text-neutral-300 md:mt-6 md:text-[1.53vw]">
+          <h2 className="text-0 mt-8 mb-11 text-center leading-[1.2] text-neutral-300 md:mt-6">
             Secure your spot at next year's biggest coding conference.
           </h2>
         </>
