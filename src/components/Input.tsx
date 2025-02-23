f2ae06477a5de284384162de334e186cf6d7ae08
kendpr <real.kendpr@gmail.com>
2025-02-23 23:22:58 +0800

    fixed label linking to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -35,6 +35,7 @@ export default function Input({
         type="text"
         {...register(name, validationRule)}
         placeholder={placeholder}
+        id={name}
       />
       {errors[name] && (
         <RequiredAlert
